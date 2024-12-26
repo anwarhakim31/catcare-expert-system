88f17fe39fcbf6444b8fe5b051dadee141b0fdb1
anwarhakim31 <anwarhakim31@gmail.com>
2024-12-26 22:03:13 +0700

    catcare finish and deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
